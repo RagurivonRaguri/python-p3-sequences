9e98e1e737a1b4f9eb8602c52437a30415def1df
Lincoln Muraguri <lincolnmuraguri93@gmail.com>
2023-05-16 14:25:50 +0300

    Done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    my_list=[0, 1]
     if length == 0:
-        print(list())
+        print([])
+        return
     elif length == 1:
-        print(list([0]))
-    elif length == 2:
-        print(list([0, 1]))
-    elif length == 10:
-        print(list([0, 1, 1, 2, 3, 5, 8, 13, 21, 34]))
+        print([0])
+        return
+    # elif length == 2:
+    #     print ([0, 1])
+    for i in range(2, length):
+        next_number = my_list[i-1] + my_list[i-2]
+        my_list.append(next_number)
+
+    print (my_list)
